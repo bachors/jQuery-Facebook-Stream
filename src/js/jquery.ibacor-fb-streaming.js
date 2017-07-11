0a4e01d1c38e0592bb96ef1619683d595930ca36
Ican Bachors <bachor.can@gmail.com>
2017-07-11 10:13:29 +0700

    v4.0 facebook api v2.9

--- src/js/jquery.ibacor-fb-streaming.js
+++ src/js/jquery.ibacor-fb-streaming.js
@@ -1,12 +1,12 @@
 /***********************************************************
-* #### jQuery-iBacor-Facebook-Streaming v3.0 ####
+* #### jQuery-Facebook-Streaming v4.0 ####
 * Coded by Ican Bachors 2014.
-* http://ibacor.com/labs/jquery-ibacor-facebook-streaming/
+* https://github.com/bachors/jQuery-Facebook-Stream
 * Updates will be posted to this site.
 ***********************************************************/
 
-$.fn.ifbs = function(n, o, p) {
-    var j = '<div class="ibacor_fb_streaming">';
+$.fn.facebook_stream = function(n, o, p) {
+    var j = '<div class="facebook-stream">';
     j += '	<div class="modalDialog">';
     j += '		<div>';
     j += '			<a href="#close" title="Close" class="close">X</a>';
@@ -15,11 +15,11 @@ $.fn.ifbs = function(n, o, p) {
     j += '	</div>';
     j += '</div>', id = ($(this).attr("id") != null || $(this).attr("id") != undefined ? '#' + $(this).attr("id") : '.' + $(this).attr("class")), limit = o + 1;
     $(this).html(j);
-    ibacor_gf(id, limit, '');
+    FS_gf(id, limit, 'until=');
 
-    function ibacor_gf(k, l, m) {
+    function FS_gf(k, l, m) {
         $.ajax({
-            url: 'https://graph.facebook.com/v2.2/' + n + '/feed?limit=' + l + '&access_token=' + p + '&until=' + m,
+            url: 'https://graph.facebook.com/v2.9/' + n + '/feed?limit=' + l + '&fields=from,message,created_time,id,likes,comments,story,link,type,name,picture,caption,description,object_id,shares&access_token=' + p + '&' + m,
             crossDomain: true,
             dataType: 'json'
         }).done(function(h) {
@@ -27,8 +27,8 @@ $.fn.ifbs = function(n, o, p) {
             $.each(h.data, function(i, a) {
                 if (i < o) {
                     j += '<div class="fb_box">';
-                    j += '<div class="fb_status"><p><a href="https://facebook.com/' + h.data[i].from.id + '" target="_BLANK"><img src ="https://graph.facebook.com/' + h.data[i].from.id + '/picture" alt="" class="fb_user_foto"> ';
-                    j += '<strong>' + h.data[i].from.name + '</strong></a><br>';
+                    j += '<div class="fb_status"><p><a href="https://facebook.com/' + h.data[i].from.id + '" target="_BLANK"><img src ="https://graph.facebook.com/v2.9/' + h.data[i].from.id + '/picture?access_token=' + p + '" alt="" class="fb_user_foto"> ';
+                    j += '' + h.data[i].from.name + '</a><br>';
                     var b = '';
                     if (h.data[i].comments == null || h.data[i].comments == undefined) {
                         b += '0'
@@ -49,7 +49,7 @@ $.fn.ifbs = function(n, o, p) {
                             c += h.data[i].likes.data.length
                         }
                     }
-                    j += '<span><i class="fa fa-clock-o"></i> ' + relative_time(h.data[i].created_time) + '</span></p>';
+                    j += '<span class="date"><i class="fa fa-clock-o"></i> ' + relative_time(h.data[i].created_time) + '</span></p>';
                     if (h.data[i].message != null || h.data[i].message != undefined) {
                         j += '<pre>' + htmlEntities(h.data[i].message) + '</pre>'
                     }
@@ -71,7 +71,7 @@ $.fn.ifbs = function(n, o, p) {
                         j += '</div></a>'
                     } else {
                         if (h.data[i].picture != null || h.data[i].picture != undefined) {
-                            j += '<a href="#openModal" data-popupimg="http://graph.facebook.com/' + h.data[i].object_id + '/picture" class="ibacor_popup" title="Zoom"><img src="http://graph.facebook.com/' + h.data[i].object_id + '/picture" alt="" class="fb_status_foto"></a><br>'
+                            j += '<a href="#openModal" data-popupimg="https://graph.facebook.com/v2.9/' + h.data[i].object_id + '/picture?access_token=' + p + '" class="FS_popup" title="Zoom"><img src="https://graph.facebook.com/v2.9/' + h.data[i].object_id + '/picture?access_token=' + p + '" alt="" class="fb_status_foto"></a><br>'
                         }
                     }
                     var d = '';
@@ -93,17 +93,19 @@ $.fn.ifbs = function(n, o, p) {
                     }
                     j += '</div><div class="tabs"><div class="tab lihatlike" data-liki="' + h.data[i].id + ',' + e + '"><i class="fa fa-thumbs-up"></i> ' + c + '</div><div class="tab lihatkom" data-kom="' + h.data[i].id + ',' + d + '"><i class="fa fa-comment"></i> ' + b + '</div><div class="tab"><a href="http://www.facebook.com/sharer/sharer.php?u=https://www.facebook.com/permalink.php?story_fbid=' + f[1] + '&id=' + n + '" target="_BLANK"><i class="fa fa-share"></i> ' + g + '</a></div><div class="tab"><a href="https://www.facebook.com/permalink.php?story_fbid=' + f[1] + '&id=' + n + '" target="_BLANK"><i class="fa fa-external-link-square"></i></a></div></div>';
                     if (h.data[i].likes != null || h.data[i].likes != undefined) {
-                        j += '<div class="fb_bls_like ibacor_like' + h.data[i].id + '" style="display:none"></div>'
+                        j += '<div class="fb_bls_like FS_like' + h.data[i].id + '" style="display:none"></div>'
                     }
                     if (h.data[i].comments != null || h.data[i].comments != undefined) {
-                        j += '<div class="fb_bls_komen ibacor_blscm' + h.data[i].id + '" style="display:none"></div>'
+                        j += '<div class="fb_bls_komen FS_blscm' + h.data[i].id + '" style="display:none"></div>'
                     }
                     j += '</div>'
                 }
             });
-            j += '<p class="load_more"><a class="btn ibacor_moregf">More</a></p>';
-            $(k + ' .ibacor_fb_streaming').append(j);
-            $('.ibacor_popup').click(function() {
+            if (h.paging.next != null || h.paging.next != undefined) {
+				j += '<p class="load_more"><a class="btn FS_moregf">More</a></p>';
+			}
+            $(k + ' .facebook-stream').append(j);
+            $('.FS_popup').click(function() {
                 $("html, body").animate({
                     scrollTop: 0
                 }, 0);
@@ -120,125 +122,135 @@ $.fn.ifbs = function(n, o, p) {
             $(k + ' .lihatlike').click(function() {
                 var a = $(this).data('liki').split(',');
                 if (a[1] > 0) {
-                    ibacor_like(a[0])
+                    FS_like(a[0])
                 }
-                $('.ibacor_like' + a[0]).css("display", "block");
-                $('.ibacor_blscm' + a[0]).css("display", "none");
+                $('.FS_like' + a[0]).css("display", "block");
+                $('.FS_blscm' + a[0]).css("display", "none");
                 return false
             });
             $(k + ' .lihatkom').click(function() {
                 var a = $(this).data('kom').split(',');
                 if (a[1] > 0) {
-                    bacor_morekomen(a[0], '', 'html')
+                    FS_morekomen(a[0], '', 'html')
                 }
-                $('.ibacor_like' + a[0]).css("display", "none");
-                $('.ibacor_blscm' + a[0]).css("display", "block");
+                $('.FS_like' + a[0]).css("display", "none");
+                $('.FS_blscm' + a[0]).css("display", "block");
                 return false
             });
-            $(k + ' .ibacor_moregf').click(function() {
-                var a = h.paging.next;
-                var b = a.split('&');
-                for (var i = 0; i < b.length; i++) {
-                    var c = b[i].split('=');
-                    if (c[0] == 'until') {
-                        ibacor_gf(k, l, c[1]);
-                        $(k + ' .load_more').remove();
-                        return false
-                    }
-                }
+            $(k + ' .FS_moregf').click(function() {
+				if(h.paging.cursors !== undefined){
+					var a = 'after=' + h.paging.cursors.after;
+					FS_gf(k, l, a);
+                    $(k + ' .load_more').remove();
+				}else{
+					var a = h.paging.next;
+					var b = a.split('&');
+					for (var i = 0; i < b.length; i++) {
+						var c = b[i].split('=');
+						if (c[0] == 'until') {
+							FS_gf(k, l, 'until=' + c[1]);
+							$(k + ' .load_more').remove();
+						}
+					}
+				}
+                return false;
             })
         })
     }
 
-    function ibacor_like(e) {
+    function FS_like(e) {
         $.ajax({
-            url: 'https://graph.facebook.com/v2.2/' + e + '/likes?access_token=' + p,
+            url: 'https://graph.facebook.com/v2.9/' + e + '/likes?access_token=' + p,
             crossDomain: true,
             dataType: 'json'
         }).done(function(b) {
             var c = '';
             $.each(b.data, function(i, a) {
-                c += '<a href="https://facebook.com/' + b.data[i].id + '" target="_BLANK" title="' + b.data[i].name + '"><img src ="https://graph.facebook.com/' + b.data[i].id + '/picture" class="likers_foto" alt=""></a>'
+                c += '<a href="https://facebook.com/' + b.data[i].id + '" target="_BLANK" title="' + b.data[i].name + '"><img src ="https://graph.facebook.com/v2.9/' + b.data[i].id + '/picture?access_token=' + p + '" class="likers_foto" alt=""></a>'
             });
             var d = '';
             if (b.data.length == 25) {
                 d += '& more '
             }
             c += '<span class="likers">' + d + 'like this.</span>';
-            $('.ibacor_like' + e).html(c)
+            $('.FS_like' + e).html(c)
         })
     }
 
-    function bacor_morekomen(e, g, h) {
+    function FS_morekomen(e, g, h) {
         $.ajax({
-            url: 'https://graph.facebook.com/v2.2/' + e + '/comments?access_token=' + p + '&after=' + g,
+            url: 'https://graph.facebook.com/v2.9/' + e + '/comments?fields=created_time,from,message,id,like_count&access_token=' + p + '&after=' + g,
             crossDomain: true,
             dataType: 'json'
         }).done(function(c) {
             var d = '';
             $.each(c.data, function(i, a) {
-                d += '<div class="fb_komen" id="komen_sub_' + c.data[i].id + '"><div class="box_komen kenca"><a href="https://facebook.com/' + c.data[i].from.id + '" target="_BLANK"><img src ="https://graph.facebook.com/' + c.data[i].from.id + '/picture" alt="" class="user_komen_foto"></a></div>';
-                d += '<div class="box_komen katuhu"><a href="https://facebook.com/' + c.data[i].from.id + '" target="_BLANK"><strong>' + c.data[i].from.name + '</strong></a>';
+                d += '<div class="fb_komen" id="komen_sub_' + c.data[i].id + '"><div class="box_komen kenca"><a href="https://facebook.com/' + c.data[i].from.id + '" target="_BLANK"><img src ="https://graph.facebook.com/v2.9/' + c.data[i].from.id + '/picture?access_token=' + p + '" alt="" class="user_komen_foto"></a></div>';
+                d += '<div class="box_komen katuhu"><a href="https://facebook.com/' + c.data[i].from.id + '" target="_BLANK">' + c.data[i].from.name + '</a>';
                 if (c.data[i].message.length > 0) {
-                    d += '<pre>' + htmlEntities(c.data[i].message) + '</pre>'
+                    d += '<p></p><pre>' + htmlEntities(c.data[i].message) + '</pre>'
                 } else {
                     d += '<br><p></p>'
                 }
                 var b = c.data[i].id;
-                d += '<p class="fb_komen_foto ibacor_comg' + b + '"></p>';
-                bacor_gambarg(b);
-                d += '<span><i class="fa fa-clock-o"></i> ' + relative_time(c.data[i].created_time) + '</span><span class="strong like_bls"><i class="fa fa-thumbs-up"></i> ' + c.data[i].like_count + '</span></div>';
+                d += '<p class="fb_komen_foto FS_comg' + b + '"></p>';
+                FS_gambarg(b);
+                d += '<span class="date"><i class="fa fa-clock-o"></i> ' + relative_time(c.data[i].created_time) + '</span><span class="strong like_bls"><i class="fa fa-thumbs-up"></i> ' + c.data[i].like_count + '</span></div>';
                 d += '</div>';
-                bacor_subkomen(c.data[i].id, '')
+                FS_subkomen(c.data[i].id, '')
             });
-            if (c.paging.next != null || c.paging.next != undefined) {
-                d += '<p class="load_more_komen"><span class="strong fcm_more' + e + '">View more comments</span></p>'
+            if (c.paging != undefined) {                
+				if (c.paging.next != undefined) {
+					d += '<p class="load_more_komen"><span class="strong fcm_more' + e + '">View more comments</span></p>'
+				}
             }
             if (h == 'html') {
-                $('.ibacor_blscm' + e).html(d)
+                $('.FS_blscm' + e).html(d)
             } else {
-                $('.ibacor_blscm' + e).append(d)
+                $('.FS_blscm' + e).append(d)
             }
             $('.fcm_more' + e).click(function() {
                 var a = c.paging.cursors.after;
                 if (a != null || a != undefined) {
-                    bacor_morekomen(e, a, 'append');
-                    $('.ibacor_blscm' + e + ' .load_more_komen').remove();
+                    FS_morekomen(e, a, 'append');
+                    $('.FS_blscm' + e + ' .load_more_komen').remove();
                     return false
                 }
             })
         })
     }
 
-    function bacor_subkomen(e, g) {
+    function FS_subkomen(e, g) {
         $.ajax({
-            url: 'https://graph.facebook.com/v2.2/' + e + '/comments?access_token=' + p + '&after=' + g,
+            url: 'https://graph.facebook.com/v2.9/' + e + '/comments?fields=created_time,from,message,id,like_count&access_token=' + p + '&after=' + g,
             crossDomain: true,
             dataType: 'json'
         }).done(function(c) {
             var d = '';
             $.each(c.data, function(i, a) {
-                d += '<div class="fb_komen_sub"><div class="box_komen kenca"><a href="https://facebook.com/' + c.data[i].from.id + '" target="_BLANK"><img src ="https://graph.facebook.com/' + c.data[i].from.id + '/picture" alt="" class="user_komen_foto"></a></div>';
-                d += '<div class="box_komen katuhu"><a href="https://facebook.com/' + c.data[i].from.id + '" target="_BLANK"><strong>' + c.data[i].from.name + '</strong></a>';
+                d += '<div class="fb_komen_sub"><div class="box_komen kenca"><a href="https://facebook.com/' + c.data[i].from.id + '" target="_BLANK"><img src ="https://graph.facebook.com/v2.9/' + c.data[i].from.id + '/picture?access_token=' + p + '" alt="" class="user_komen_foto"></a></div>';
+                d += '<div class="box_komen katuhu"><a href="https://facebook.com/' + c.data[i].from.id + '" target="_BLANK">' + c.data[i].from.name + '</a>';
                 if (c.data[i].message.length > 0) {
-                    d += '<pre>' + htmlEntities(c.data[i].message) + '</pre>'
+                    d += '<p></p><pre>' + htmlEntities(c.data[i].message) + '</pre>'
                 } else {
                     d += '<br><p></p>'
                 }
                 var b = c.data[i].id;
-                d += '<p class="fb_komen_foto ibacor_comg' + b + '"></p>';
-                bacor_gambarg(b);
-                d += '<span><i class="fa fa-clock-o"></i> ' + relative_time(c.data[i].created_time) + '</span><span class="strong like_bls"><i class="fa fa-thumbs-up"></i> ' + c.data[i].like_count + '</span></div>';
+                d += '<p class="fb_komen_foto FS_comg' + b + '"></p>';
+                FS_gambarg(b);
+                d += '<span class="date"><i class="fa fa-clock-o"></i> ' + relative_time(c.data[i].created_time) + '</span><span class="strong like_bls"><i class="fa fa-thumbs-up"></i> ' + c.data[i].like_count + '</span></div>';
                 d += '</div>'
             });
-            if (c.paging.next != null || c.paging.next != undefined) {
-                d += '<p class="load_more_komen_sub"><span class="strong fcm_moresub">View more comments</span></p>'
+            if (c.paging != undefined) {
+				if (c.paging.next != undefined) {
+					d += '<p class="load_more_komen_sub"><span class="strong fcm_moresub">View more comments</span></p>'
+				}
             }
             $('#komen_sub_' + e).append(d);
             $('.fcm_moresub').click(function() {
                 var a = c.paging.cursors.after;
                 if (a != null || a != undefined) {
-                    bacor_subkomen(e, a);
+                    FS_subkomen(e, a);
                     $('#komen_sub_' + e + ' .load_more_komen_sub').remove();
                     return false
                 }
@@ -246,18 +258,18 @@ $.fn.ifbs = function(n, o, p) {
         })
     }
 
-    function bacor_gambarg(c) {
+    function FS_gambarg(c) {
         $.ajax({
-            url: 'https://graph.facebook.com/v2.2/' + c + '?fields=attachment&access_token=' + p,
+            url: 'https://graph.facebook.com/v2.9/' + c + '?fields=attachment&access_token=' + p,
             crossDomain: true,
             dataType: 'json'
         }).done(function(a) {
             var b = '';
             if (a.attachment != null || a.attachment != undefined) {
-                b += '<a data-popupimg="' + a.attachment.media.image.src + '" class="ibacor_popup" title="Zoom"><img src="' + a.attachment.media.image.src + '" alt=""></a>'
+                b += '<a data-popupimg="' + a.attachment.media.image.src + '" class="FS_popup" title="Zoom"><img src="' + a.attachment.media.image.src + '" alt=""></a>'
             }
-            $('.ibacor_comg' + c).html(b);
-            $('.ibacor_popup').click(function() {
+            $('.FS_comg' + c).html(b);
+            $('.FS_popup').click(function() {
                 $("html, body").animate({
                     scrollTop: 0
                 }, 0);
@@ -275,7 +287,7 @@ $.fn.ifbs = function(n, o, p) {
     }
 
     function htmlEntities(a) {
-        return ibacor_urltag(String(a).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;'))
+        return FS_urltag(String(a).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;'))
     }
 
     function relative_time(a) {
@@ -310,7 +322,7 @@ $.fn.ifbs = function(n, o, p) {
         return r
     }
 
-    function ibacor_urltag(c, d) {
+    function FS_urltag(c, d) {
         var e = {
             lt: {
                 regex: /(<)/g,
